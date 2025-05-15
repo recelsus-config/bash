f57745deda7ccbf380bf9ab0fb0e357c3c932617
recelsus <rhinfunc+git@gmail.com>
2025-05-16 00:10:34 +0900

    Fix: Use GITHUB_TOKEN and GITHUB_USER for authenticated git clone
    - scripts/git.sh: Use provided user and token for authentication in git clone

--- scripts/git.sh
+++ scripts/git.sh
@@ -32,23 +32,24 @@ git-update() {
 git-clone() {
   if [ -z "$GITHUB_TOKEN" ] || [ -z "$GITHUB_USER" ]; then
     echo "[ERROR] GITHUB_TOKEN or GITHUB_USER is not set." >&2
-
     return 1
   fi
+
+  local user="${GITHUB_USER//\"/}"
+  local token="${GITHUB_TOKEN//\"/}"
 
   local repo_url="$1"
   local auth_url=""
 
   if [[ "$repo_url" =~ ^https://github\.com/ ]]; then
-    auth_url=$(echo "$repo_url" | sed "s|^https://|https://${GITHUB_USER}:${GITHUB_TOKEN}@|")
+    auth_url="https://${user}:${token}@${repo_url#https://}"
 
   elif [[ "$repo_url" =~ ^git@github\.com: ]]; then
-    local https_url="https://github.com/${repo_url#git@github.com:}"
-    auth_url="https://${GITHUB_USER}:${GITHUB_TOKEN}@${https_url#https://}"
+    local path="${repo_url#git@github.com:}"
+    auth_url="https://${user}:${token}@github.com/${path}"
 
   else
     echo "[ERROR] Unsupported URL format: $repo_url" >&2
-
     return 2
   fi
 
