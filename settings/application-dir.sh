6cea1193fdebae1cc5d73d5c4f600b6ae14068fc
recelsus <rhinfunc+git@gmail.com>
2025-05-18 21:45:49 +0900

    Move GnuPG and add more XDG data directory settings.
    - settings/application-dir.sh: Moved GnuPG config and added Go, Cargo, Conan and Rustup settings.

--- settings/application-dir.sh
+++ settings/application-dir.sh
@@ -1,7 +1,5 @@
 # vim: ft=sh
 
-export AICHAT_CONFIG_DIR="$HOME"/.config/aichat
-export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
 export RANDFILE="$XDG_CONFIG_HOME"/openssl/rnd
@@ -11,6 +9,8 @@ export HISTFILE="$XDG_STATE_HOME"/bash/history
 export BASH_SESSION="$XDG_STATE_HOME/bash/sessions"
 export TERMINFO="$XDG_DATA_HOME"/terminfo
 
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export GOPATH="$XDG_DATA_HOME"/go
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
-
+export CONAN_USER_HOME="$XDG_DATA_HOME"/conan
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
